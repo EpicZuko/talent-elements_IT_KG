f39672fc41b7a96e6bfeb7f103b824902e053f82
amanbaevaanarbu <amaanarbu633@gmail.com>
2023-08-10 23:14:47 +0300

    created staff admin card groups

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import loginOrRegisterSlice from './reducerSlice/authSlice/loginOrRegisterSlice'
 import getSeoAdminGroupSlice from './reducerSlice/seoAdminGroupsSlice/allGroups'
+import staffAdminSlice from './reducerSlice/staffAdminSlice/staffAdmin'
 
 const store = configureStore({
   reducer: {
     login: loginOrRegisterSlice.reducer,
     seoAdmin: getSeoAdminGroupSlice.reducer,
+    staffAdmin: staffAdminSlice.reducer,
   },
 })
 export default store
