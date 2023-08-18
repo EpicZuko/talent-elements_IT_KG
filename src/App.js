7a7b31281642c922de3ac8d4045f936a022e5f38
amanbaevaanarbu <amaanarbu633@gmail.com>
2023-08-18 17:57:44 +0300

    created staff admin studetn id home work delete routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import StaffAdminRouts from './routes/StaffAdminRouts'
-
 function App() {
-  return (
-    <div>
-      <StaffAdminRouts />
-    </div>
-  )
+  return <div />
 }
 export default App
